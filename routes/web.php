5825c19815a510b190ecbf2089eb36da455bbea3
Bhalchandra <nehabhal.work@gmail.com>
2026-07-02 19:50:16 +0530

    route change

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 })->name('home');
 Route::get('/home', function () {
