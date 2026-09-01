cd22ba1ff97a55495127aa577b2433a0a79d8c27
nehhaa241 <cneha0801@gmail.com>
2026-09-01 14:59:01 +0530

    parichay updated

--- resources/views/parichay.blade.php
+++ resources/views/parichay.blade.php
@@ -1,30 +1,32 @@
-<div id="parichay" class="container-xxl py-5">
-    <div class="container">
-        <div class="row g-5">
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
-                <div class="about-img">
-                    <img class="img-fluid w-100 mb-3" src="{{ asset('img/IMG_7945.jpg') }}" alt="">
-                    <img class="img-fluid w-100" src="{{ asset('img/IMG_7575.jpg') }}" alt="">
-                </div>
-            </div>
-            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                <h4 class="section-title">परिचय</h4>
-                <h1 class="display-5 mt-4">महासंघाबद्दल</h1>
-                <p>मराठी बांधकाम व्यावसायिक महासंघ ही महाराष्ट्रातील मराठी विकासक व बांधकाम क्षेत्राशी संबंधित सर्व
-                    घटकांची राज्यस्तरीय शिखर संघटना आहे. प्रत्येक जिल्ह्यात असोसिएशन स्थापन करून सर्व विकासकांना
-                    एकत्रित व्यासपीठ देणे आणि शासन व उद्योग यांच्यात प्रभावी समन्वय निर्माण करणे हे महासंघाचे ध्येय
-                    आहे.</p>
-                <div class="d-flex flex-column flex-sm-row align-items-center mb-3 mt-5 text-center text-sm-start">
-                    <div class="d-flex flex-shrink-0 align-items-center justify-content-center border border-4 border-primary mb-3 mb-sm-0"
-                        style="width: 100px; height: 100px; border-radius: 16px;">
-                        <h1 class="display-3 mb-n2">२५</h1>
-                    </div>
-                    <div class="ps-sm-4">
-                        <h3 style="font-weight: 600!important;">वर्षांचा कामाचा अनुभव</h3>
-                    </div>
-                </div>
-                <a class="btn btn-primary py-3 mt-4 px-5 w-100 w-sm-auto" href="{{ route('parichay-details') }}">अधिक वाचा</a>
-            </div>
-        </div>
-    </div>
-</div>
+ <div id="parichay" class="container-xxl py-5">
+     <div class="container">
+         <div class="row g-5">
+             <div class="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                 <div class="about-img">
+                     <img class="img-fluid" src="img/IMG_7945.jpg" alt="">
+                     <img class="img-fluid" src="img/IMG_7575.jpg" alt="">
+                 </div>
+             </div>
+             <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                 <h4 class="section-title">परिचय</h4>
+                 <h1 class="display-5 mt-4">महासंघाबद्दल</h1>
+                 <p>मराठी बांधकाम व्यावसायिक महासंघ ही महाराष्ट्रातील मराठी विकासक व बांधकाम क्षेत्राशी संबंधित सर्व
+                     घटकांची राज्यस्तरीय शिखर संघटना आहे. प्रत्येक जिल्ह्यात असोसिएशन स्थापन करून सर्व विकासकांना
+                     एकत्रित व्यासपीठ देणे आणि शासन व उद्योग यांच्यात प्रभावी समन्वय निर्माण करणे हे महासंघाचे ध्येय
+                     आहे.</p>
+                 <div class="d-flex align-items-center  mb-3 mt-5">
+                     <div class="d-flex flex-shrink-0 align-items-center justify-content-center  border border-4 border-primary"
+                         style="width: 120px; height: 120px; border-radius: 16px;">
+                         <h1 class="display-1 mb-n2" data-toggle1="counter-up">२५</h1>
+                     </div>
+                     <div class="ps-4">
+                         <h3 style="font-weight: 600!important;"> वर्षांचा कामाचा अनुभव</h3>
+                         {{-- <h3>कामाचा</h3>
+                         <h3 class="mb-0">अनुभव</h3> --}}
+                     </div>
+                 </div>
+                 <a class="btn btn-primary py-3 mt-4 px-5" href="{{ route('parichay-details') }}">अधिक वाचा</a>
+             </div>
+         </div>
+     </div>
+ </div>
